b51d71929cb147a8e3f713be4822f46dc16a9dcf
HYLee1008 <discone1008@gmail.com>
2021-04-27 08:33:50 +0000

    feat: utility functions for training & inference

---
+++ utils.py
@@ -0,0 +1,135 @@
+# https://github.com/wkentaro/pytorch-fcn/blob/master/torchfcn/utils.py
+import numpy as np
+import pandas as pd
+import glob
+from pathlib import Path
+import torch
+import random
+import re
+import sys
+import os
+
+
+def _fast_hist(label_true, label_pred, n_class):
+    mask = (label_true >= 0) & (label_true < n_class)
+    hist = np.bincount(
+        n_class * label_true[mask].astype(int) +
+        label_pred[mask], minlength=n_class ** 2).reshape(n_class, n_class)
+    return hist
+
+
+def label_accuracy_score(label_trues, label_preds, n_class):
+    """Returns accuracy score evaluation result.
+      - overall accuracy
+      - mean accuracy
+      - mean IU
+      - fwavacc
+    """
+    hist = np.zeros((n_class, n_class))
+    for lt, lp in zip(label_trues, label_preds):
+        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
+    acc = np.diag(hist).sum() / hist.sum()
+    with np.errstate(divide='ignore', invalid='ignore'):
+        acc_cls = np.diag(hist) / hist.sum(axis=1)
+    acc_cls = np.nanmean(acc_cls)
+    with np.errstate(divide='ignore', invalid='ignore'):
+        iu = np.diag(hist) / (
+            hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)
+        )
+    mean_iu = np.nanmean(iu)
+    freq = hist.sum(axis=1) / hist.sum()
+    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+    return acc, acc_cls, mean_iu, fwavacc
+
+
+def increment_path(path, exist_ok=False):
+    """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+
+    Args:
+        path (str or pathlib.Path): f"{model_dir}/{args.name}".
+        exist_ok (bool): whether increment path (increment if False).
+    """
+    path = Path(path)
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}*")
+        matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]
+        n = max(i) + 1 if i else 2
+        return f"{path}{n}"
+
+
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+def get_category_names(dataset):
+    categories = dataset['categories']
+    anns = dataset['annotations']
+    imgs = dataset['images']
+    nr_cats = len(categories)
+    nr_annotations = len(anns)
+    nr_images = len(imgs)
+
+    # Load categories and super categories
+    cat_names = []
+    super_cat_names = []
+    super_cat_ids = {}
+    super_cat_last_name = ''
+    nr_super_cats = 0
+    for cat_it in categories:
+        cat_names.append(cat_it['name'])
+        super_cat_name = cat_it['supercategory']
+        # Adding new supercat
+        if super_cat_name != super_cat_last_name:
+            super_cat_names.append(super_cat_name)
+            super_cat_ids[super_cat_name] = nr_super_cats
+            super_cat_last_name = super_cat_name
+            nr_super_cats += 1
+
+    # Count annotations
+    cat_histogram = np.zeros(nr_cats,dtype=int)
+    for ann in anns:
+        cat_histogram[ann['category_id']] += 1
+
+    # class categories
+    df = pd.DataFrame({'Categories': cat_names, 'Number of annotations': cat_histogram})
+    df = df.sort_values('Number of annotations', 0, False)
+
+    # category labeling 
+    sorted_temp_df = df.sort_index()
+
+    # background = 0 에 해당되는 label 추가 후 기존들을 모두 label + 1 로 설정
+    sorted_df = pd.DataFrame(["Backgroud"], columns = ["Categories"])
+    sorted_df = sorted_df.append(sorted_temp_df, ignore_index=True)
+
+    category_names = list(sorted_df.Categories)
+
+    return category_names
+
+
+class Logger(object):
+    def __init__(self, save_dir):
+        self.terminal = sys.__stdout__
+        self.log = open(os.path.join(save_dir, 'log.txt'), "w", encoding='utf-8')
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)  
+
+    def flush(self):
+        #this flush method is needed for python 3 compatibility.
+        #this handles the flush command by doing nothing.
+        #you might want to specify some extra behavior here.
+        pass    
+
+    def __del__(self): 
+        # for file close
+        self.log.close() 
