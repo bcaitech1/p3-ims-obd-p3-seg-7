2dec4fd522c994d8706d139c5209f3970fc90eb9
HYLee1008 <discone1008@gmail.com>
2021-05-03 13:47:34 +0000

    <docs> add several augmentation
    several augmentation added

--- augmentation.py
+++ augmentation.py
@@ -33,9 +33,10 @@ class ImagenetDefaultAugmentation:
 class CustomAugmentation:
     def __init__(self):
         self.transform = A.Compose([
+            A.Resize(512, 512),
             A.HorizontalFlip(0.5),
             A.VerticalFlip(0.5),
-            A.Rotate(),
+            A.Rotate(p=0.5),
             # A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ToTensorV2(),
         ])
@@ -43,13 +44,44 @@ class CustomAugmentation:
     def __call__(self, **data):
         return self.transform(**data)
 
+class CustomAugmentation2:
+    def __init__(self):
+        self.transform = A.Compose([
+            A.Resize(512, 512),
+            A.HorizontalFlip(0.5),
+            A.VerticalFlip(0.5),
+            A.ShiftScaleRotate(p=0.5),
+            # A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ToTensorV2(),
+        ])
+
+    def __call__(self, **data):
+        return self.transform(**data)
+
+
 class CustomElasticAugmentation:
     def __init__(self, alpha=512*2, sigma=512*0.08, alpha_affine=512*0.08):
         self.transform = A.Compose([
             A.HorizontalFlip(0.5),
             A.VerticalFlip(0.5),
             A.Rotate(),
-            ElasticTransform(alpha, sigma, alpha_affine, p=1),
+            ElasticTransform(alpha, sigma, alpha_affine, p=0.5),
+            # A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ToTensorV2(),
+        ])
+
+    def __call__(self, **data):
+        return self.transform(**data)
+
+
+class CustomGridDistortionAugmentation:
+    def __init__(self):
+        self.transform = A.Compose([
+            A.Resize(512, 512),
+            A.HorizontalFlip(0.5),
+            A.VerticalFlip(0.5),
+            A.Rotate(p=0.5),
+            A.GridDistortion(),
             # A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ToTensorV2(),
         ])
